94f4aafc6fd37a22bfabd7f2e6fc6521f022ee3d
Vishal Bidikar <v7vishalbidikar@gmail.com>
2024-04-30 10:06:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import Header from "./components/Header.jsx";
+import Results from "./components/Results.jsx";
+import UserInput from "./components/UserInput.jsx";
+import { useState } from "react";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration >=1;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+  
+
+  return (
+    <>
+     <Header />
+     <UserInput userInput={userInput} onChange={handleChange} />
+     {!inputIsValid && <p className="center">Please Enter Valid Duration greater than Zero</p>}
+     {inputIsValid && <Results input={userInput}/>}
+    </>
+  );
+}
+
+export default App
